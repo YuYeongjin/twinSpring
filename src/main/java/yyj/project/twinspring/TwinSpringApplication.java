8200afc111241146733f5921686789e36f1d2c55
user <svvo2002@naver.com>
2025-05-27 21:31:42 +0900

    first commit

---
+++ src/main/java/yyj/project/twinspring/TwinSpringApplication.java
@@ -0,0 +1,13 @@
+package yyj.project.twinspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwinSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwinSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/yyj/project/twinspring/TwinSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package yyj.project.twinspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwinSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
