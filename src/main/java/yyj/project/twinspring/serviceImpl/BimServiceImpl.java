f88b835005cb955561e3dbdf47ae221857439e9d
user <svvo2002@naver.com>
2025-10-11 22:26:26 +0900

    clean url

--- src/main/java/yyj/project/twinspring/serviceImpl/BimServiceImpl.java
+++ src/main/java/yyj/project/twinspring/serviceImpl/BimServiceImpl.java
@@ -1,15 +1,16 @@
 package yyj.project.twinspring.serviceImpl;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
-import yyj.project.twinspring.dto.BimModelDTO;
 import yyj.project.twinspring.service.BimService;
 
 @Service
 public class BimServiceImpl implements BimService {
 
-    private static final String CSHARP_BASE_URL = "http://localhost:5112";
+    @Value("bim.server.url")
+    private static String CSHARP_BASE_URL;
 
     private final WebClient webClient;
 
@@ -20,11 +21,11 @@ public class BimServiceImpl implements BimService {
     @Override
     public Mono<String> getModelData(String projectId) {
 
-        // C# 서버의 /api/bim/model 엔드포인트를 호출
+        // C# 서버 /api/bim/model 호출
         return webClient.get()
                 .uri("/api/bim/model/{projectId}", projectId)
                 .retrieve()
-                .bodyToMono(String.class) // C# 응답을 DTO로 자동 변환
+                .bodyToMono(String.class)
                 .doOnError(e -> System.err.println("C# 서버 통신 오류: " + e.getMessage()));
     }
 }
