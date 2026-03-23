7a46b8a858e26bcc81a36170a447133449bb652e
YuYeongjin <svvo2002@naver.com>
2026-03-23 17:01:07 +0900

    update v1 debug ing

--- src/main/java/yyj/project/twinspring/config/MqttConfig.java
+++ src/main/java/yyj/project/twinspring/config/MqttConfig.java
@@ -1,7 +1,10 @@
 package yyj.project.twinspring.config;
 
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.annotation.ServiceActivator;
@@ -17,18 +20,24 @@ import yyj.project.twinspring.service.MqttService;
 @Configuration
 public class MqttConfig {
 
+    private static final Logger log = LoggerFactory.getLogger(MqttConfig.class);
+
     @Autowired
     public MqttService mqttService;
+
+    @Value("${mqtt.broker.url:tcp://192.168.219.103:1883}")
+    private String brokerUrl;
+
+    @Value("${mqtt.topic:test/topic}")
+    private String mqttTopic;
 
     @Bean
     public MqttPahoClientFactory mqttClientFactory() {
         DefaultMqttPahoClientFactory factory = new DefaultMqttPahoClientFactory();
-        MqttConnectOptions options   = new MqttConnectOptions();
-        options.setServerURIs(new String[]{"tcp://192.168.219.103:1883"});
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setServerURIs(new String[]{brokerUrl});
         options.setCleanSession(true);
-
         factory.setConnectionOptions(options);
-
         return factory;
     }
 
@@ -37,31 +46,22 @@ public class MqttConfig {
         return new DirectChannel();
     }
 
-//    @Bean
-//    public MessageProducerSupport mqttInbound() {
-//        MqttPahoMessageDrivenChannelAdapter adapter =
-//                new MqttPahoMessageDrivenChannelAdapter("testClient", mqttClientFactory(),
-//                        "test/topic");
-//        adapter.setOutputChannel(mqttInputChannel());
-//        return adapter;
-//    }
-
     @Bean
     public MessageProducerSupport mqttInbound() {
-        // ID 뒤에 현재 시간을 붙여 중복 방지
-        String uniqueClientId = "testClient_" + System.currentTimeMillis();
+        // 재시작 시 클라이언트 ID 충돌을 방지하기 위해 타임스탬프 추가
+        String clientId = "twinspring-client_" + System.currentTimeMillis();
         MqttPahoMessageDrivenChannelAdapter adapter =
-                new MqttPahoMessageDrivenChannelAdapter(uniqueClientId, mqttClientFactory(), "test/topic");
+                new MqttPahoMessageDrivenChannelAdapter(clientId, mqttClientFactory(), mqttTopic);
         adapter.setOutputChannel(mqttInputChannel());
         return adapter;
     }
+
     @Bean
     @ServiceActivator(inputChannel = "mqttInputChannel")
     public MessageHandler handler() {
         return message -> {
-            System.out.println("Received message: " + message.getPayload());
             String payload = (String) message.getPayload();
-
+            log.debug("MQTT 메시지 수신: {}", payload);
             mqttService.handleMessage(payload);
         };
     }
