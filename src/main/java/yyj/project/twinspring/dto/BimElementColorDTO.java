cb500116761167b939d49a923b1831c21e906ff2
YuYeongjin <svvo2002@naver.com>
2026-04-06 08:59:18 +0900

    bim upgrade..

---
+++ src/main/java/yyj/project/twinspring/dto/BimElementColorDTO.java
@@ -0,0 +1,13 @@
+package yyj.project.twinspring.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BimElementColorDTO {
+    private String elementId;
+    private String projectId;
+    private String color;
+}
